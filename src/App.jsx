4b2e90d2c60848d14d60a3efe139d31b92c83c2f
DevSheila <devsheilawambui@gmail.com>
2025-01-22 14:21:23 +0300

    Create dashboard page starter code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Button } from "@/components/ui/button";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import Dashboard from "./app/Dashboard/Dashboard";
+import NotFound from "./app/NotFound/NotFound";
+
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-green-300">
-        Hello world!
-      </h1>
-      <div>
-      <Button>Click me</Button>
-    </div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
+         
+        </Routes>
+      </Router>
     </>
   );
 }
