9a96ad2893f39d0834170bf1d08609c3f14eb812
DevSheila <devsheilawambui@gmail.com>
2025-01-23 12:12:32 +0300

    Implement weather forecasting feature with openweatherapi

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Dashboard from "./app/Dashboard/Dashboard";
 import LandingPage from "./app/LandingPage/LandingPage";
 import SpinLoader from "./elements/Loaders/SpinLoader";
 import { useUser } from "@clerk/clerk-react";
+import WeatherForecasting from "./app/WeatherForecasting/WeatherForecasting";
 
 const PrivateRoute = ({ element }) => {
   const { isLoaded, isSignedIn } = useUser();
@@ -45,6 +46,8 @@ function App() {
             path="/dashboard"
             element={<PrivateRoute element={<Dashboard />} />} //Protected route
           />
+          <Route exact path="/weather" element={<WeatherForecasting />} />
+
 
           <Route path="*" element={<NotFound />} />
         </Routes>
