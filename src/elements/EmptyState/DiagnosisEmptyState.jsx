b7459d3afa454ec6a23000f09ad6a4dcbfe19159
DevSheila <devsheilawambui@gmail.com>
2025-01-27 14:03:07 +0300

    Add padding

--- src/elements/EmptyState/DiagnosisEmptyState.jsx
+++ src/elements/EmptyState/DiagnosisEmptyState.jsx
@@ -4,19 +4,19 @@ import React from "react";
 function DiagnosisEmptyState() {
   return (
     <>
-      <div className="absolute left-1/2 right-1/2 flex  w-full -translate-x-1/2 transform flex-col  md:w-1/2 mb-10">
+      <div className="absolute left-1/2 right-1/2 flex  w-full -translate-x-1/2 transform flex-col  md:w-1/2 mb-10 px-4">
         <div className="text-center">
           <img
             src="/plantcare1.svg"
             className="mx-auto w-3/4 h-3/4"
-            alt="No Diagnosis yet"
+            alt="No Diagnosis yet" 
           />
 
           <h1 className=" text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Grow Healthy
           </h1>
 
-          <p className="mt-4 text-gray-500">
+          <p className="mt-4 text-gray-500 px-4">
             Don’t leave your plant’s health to chance!we’re here to help every
             step of the way.
           </p>
